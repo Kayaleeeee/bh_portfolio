4e0a41d7c68453265111c2c8454e444f4a7a9da6
Kaya Lee <gayeon71057@gmail.com>
2023-01-25 00:15:17 +0900

    film detail 페이지 추가

--- src/pages/Film/FilmDetailPage.tsx
+++ src/pages/Film/FilmDetailPage.tsx
@@ -1,9 +1,69 @@
 import { PageContainer } from "components/PageContainer";
-import React from "react";
-// import { useParams } from "react-router-dom";
+import React, { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { Device } from "utils/viewPort";
+import { getFilmData } from "./data";
 
 export const FilmDetailPage = () => {
-  // const { id } = useParams();
+  const { id } = useParams();
 
-  return <PageContainer></PageContainer>;
+  const filmData = useMemo(() => {
+    return getFilmData(Number(id));
+  }, [id]);
+
+  return (
+    <PageContainer>
+      {!filmData ? (
+        <></>
+      ) : (
+        <Wrapper>
+          <Title>{filmData.title}</Title>
+          <ImageList>
+            {filmData.sceenImageSrcList.map((imageSource) => {
+              return (
+                <Image
+                  key={imageSource}
+                  src={require(`images/film${imageSource}`)}
+                />
+              );
+            })}
+          </ImageList>
+        </Wrapper>
+      )}
+    </PageContainer>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Nanum Myeongjo", serif;
+`;
+
+const Title = styled.div`
+  color: white;
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  font-wright: 400;
+
+  @media screen and ${Device.mobile} {
+    font-size: 1.5rem;
+    margin: 0 0.5rem 2rem 0.5rem;
+  }
+`;
+
+const ImageList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  max-width: 1024px;
+  width: 100%;
+  margin-bottom: 2rem;
+`;
--- src/pages/Film/FilmListPage.tsx
+++ src/pages/Film/FilmListPage.tsx
@@ -1,10 +1,30 @@
 import { PageContainer } from "components/PageContainer";
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export const FilmListPage = () => {
   const navigate = useNavigate();
+
+  const filmList = [
+    { name: "있는힘껏", src: require("images/film/film_1/main.JPG") },
+    {
+      name: "불안은 영혼을 잠식한다",
+      src: require("images/film/film_2/main.jpeg"),
+    },
+    {
+      name: "황금마차 떠났다",
+      src: require("images/film/film_3/main.jpeg"),
+    },
+    {
+      name: "너의 기억",
+      src: require("images/film/film_4/main.jpeg"),
+    },
+    {
+      name: "출장",
+      src: require("images/film/film_5/main.jpeg"),
+    },
+  ];
 
   const onClickThumnail = (id: number) => {
     navigate(`/film/${id}`);
@@ -12,14 +32,46 @@ export const FilmListPage = () => {
 
   return (
     <PageContainer>
-      <VideoList></VideoList>
+      <FilmList>
+        {filmList.map((film, index) => {
+          return (
+            <ThumnailImage
+              key={film.name}
+              src={film.src}
+              onClick={() => onClickThumnail(index + 1)}
+            />
+          );
+        })}
+      </FilmList>
     </PageContainer>
   );
 };
 
-const VideoList = styled.div`
+const FilmList = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  align-items: baseline;
+`;
+
+const ThumnailImage = styled.img`
+  width: 100%;
+  height: auto;
+
+  &:nth-child(1) {
+    width: 50%;
+  }
+
+  &:nth-child(2) {
+    width: 50%;
+  }
+
+  &:nth-child(3) {
+    width: 50%;
+  }
+
+  &:nth-child(4) {
+    grid-column-start: 2;
+    grid-column-end: 4;
+  }
 `;
